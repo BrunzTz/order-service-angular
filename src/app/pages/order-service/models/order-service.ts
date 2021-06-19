96f23fe3b1d37aa2f6a3146d250b247c9b960636
Bruno Rocha <brunolongo333@hotmail.com>
2021-06-19 17:40:36 -0300

    Adicionando novo campo a interface

--- src/app/pages/order-service/models/order-service.ts
+++ src/app/pages/order-service/models/order-service.ts
@@ -1,13 +1,15 @@
 import { Convenio } from "./convenio";
+import { Exame } from "./exame";
 import { Medico } from "./medico";
 import { Paciente } from "./paciente";
 import { PostoColeta } from "./posto-coleta";
 
-export interface OrderServico {
+export interface OrderService {
     id: number,
     data: Date,
     paciente: Paciente,
     convenio: Convenio,
     postoColeta: PostoColeta,
-    medico: Medico
+    medico: Medico,
+    exame: Exame
 }
